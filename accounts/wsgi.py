7cedd796c64076d02befef641c23464a52a55908
“Edwin574” <owinoedwin574@gmail.com>
2025-04-09 22:32:27 +0300

    massive architecture change

--- accounts/wsgi.py
+++
@@ -1,8 +0,0 @@
-import os
-from django.core.wsgi import get_wsgi_application
-
-# Point to the settings of the accounts app
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'accounts.settings')
-
-# Get the WSGI application callable
-application = get_wsgi_application()
